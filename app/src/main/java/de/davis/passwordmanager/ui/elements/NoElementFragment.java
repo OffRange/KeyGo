7fc30f6ac4adfa5477ead78df0fb56c13d01d310
Davis <davis-wolfermann2005@gmx.de>
2023-04-10 17:39:00 +0200

    [SEViews] Added no element selected view

---
+++ app/src/main/java/de/davis/passwordmanager/ui/elements/NoElementFragment.java
@@ -0,0 +1,12 @@
+package de.davis.passwordmanager.ui.elements;
+
+import androidx.fragment.app.Fragment;
+
+import de.davis.passwordmanager.R;
+
+public class NoElementFragment extends Fragment {
+
+    public NoElementFragment() {
+        super(R.layout.fragment_no_item_selected);
+    }
+}
