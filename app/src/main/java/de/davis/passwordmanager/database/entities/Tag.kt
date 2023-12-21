daf5282bc7d26a5f262e07a01a6d0ac21c0ab573
Davis Wolfermann <davis-wolfermann2005@gmx.de>
2023-12-22 00:06:10 +0100

    [Tag] Converted to data class

--- app/src/main/java/de/davis/passwordmanager/database/entities/Tag.kt
+++ app/src/main/java/de/davis/passwordmanager/database/entities/Tag.kt
@@ -1,13 +1,15 @@
 package de.davis.passwordmanager.database.entities
 
+import android.content.Context
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import de.davis.passwordmanager.database.ElementType
 import de.davis.passwordmanager.database.TAG_PREFIX
 import de.davis.passwordmanager.gson.annotations.Exclude
 
 @Entity(indices = [Index("name", unique = true)])
-class Tag @JvmOverloads constructor(
+data class Tag @JvmOverloads constructor(
     val name: String,
     @Exclude @PrimaryKey(autoGenerate = true) val tagId: Long = 0
 )
@@ -17,3 +19,6 @@ fun Collection<Tag>.onlyCustoms(): Collection<Tag> {
 }
 
 val Tag.shouldBeProtected get() = this.name.startsWith(TAG_PREFIX)
+
+fun Tag.getLocalizedName(context: Context) =
+    if (shouldBeProtected) context.getString(ElementType.entries.first { e -> e.tag.name == name }.title) else name
