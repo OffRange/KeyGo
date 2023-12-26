29113307fd1038e7f1ce8cfc39c6c58d25332aa3
Davis Wolfermann <davis-wolfermann2005@gmx.de>
2023-12-26 12:24:08 +0100

    [Internal] Updated AGP to 8.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,6 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
