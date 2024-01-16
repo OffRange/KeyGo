66cf0de079b8795811a30902e9a634e057c8a2ab
Davis <davis-wolfermann2005@gmx.de>
2024-01-16 17:51:43 +0100

    [Autofill Service] Deleted old autofill files

--- app/src/main/java/de/davis/passwordmanager/ui/auth/BasicAuthenticationFragment.kt
+++ app/src/main/java/de/davis/passwordmanager/ui/auth/BasicAuthenticationFragment.kt
@@ -1,21 +1,12 @@
 package de.davis.passwordmanager.ui.auth
 
 import android.app.Activity
-import android.app.assist.AssistStructure
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.service.autofill.FillRequest
-import android.service.autofill.FillResponse
-import android.view.autofill.AutofillManager
-import android.view.autofill.AutofillManager.EXTRA_AUTHENTICATION_RESULT
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import de.davis.passwordmanager.ktx.getParcelableCompat
 import de.davis.passwordmanager.security.mainpassword.MainPassword
-import de.davis.passwordmanager.service.ParsedStructure
-import de.davis.passwordmanager.service.Response
 import de.davis.passwordmanager.ui.MainActivity
 
 open class BasicAuthenticationFragment : Fragment() {
@@ -41,21 +32,14 @@ open class BasicAuthenticationFragment : Fragment() {
 
     protected fun success() {
         val intent = Intent()
-        var autoFill = false
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createAutofillResponse()?.let {
-                intent.putExtra(EXTRA_AUTHENTICATION_RESULT, it)
-                autoFill = true
-            }
+
+        authenticationRequest.additionalExtras?.let {
+            intent.putExtras(it)
+            requireActivity().setResult(
+                Activity.RESULT_OK,
+                intent
+            )
         }
-
-        if (!autoFill)
-            authenticationRequest.additionalExtras?.let { intent.putExtras(it) }
-
-        requireActivity().setResult(
-            Activity.RESULT_OK,
-            intent
-        )
 
         if (requireActivity().intent.action == Intent.ACTION_MAIN)
             startActivity(Intent(requireContext(), MainActivity::class.java))
@@ -63,28 +47,5 @@ open class BasicAuthenticationFragment : Fragment() {
             authenticationRequest.intent?.let { startActivity(it) }
 
         requireActivity().finish()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun createAutofillResponse(): FillResponse? {
-        val assistStructure: AssistStructure? =
-            requireActivity().intent.extras?.getParcelableCompat(
-                AutofillManager.EXTRA_ASSIST_STRUCTURE,
-                AssistStructure::class.java
-            )
-
-        val fillRequest: FillRequest? =
-            authenticationRequest.additionalExtras?.getParcelableCompat(
-                Response.EXTRA_FILL_REQUEST,
-                FillRequest::class.java
-            )
-
-        if (fillRequest == null || assistStructure == null) return null
-
-        return Response(
-            requireContext(),
-            ParsedStructure.parse(assistStructure, requireContext()),
-            fillRequest
-        ).createRealResponse()
     }
 }
