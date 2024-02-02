dae717ffd0f0fbb6f197e38912ae0e95f5d7d133
Davis <davis-wolfermann2005@gmx.de>
2024-02-03 00:53:26 +0100

    [LoadingDialog] Enhanced Progress Handling and Control
    Refactored LoadingDialog in the app's UI components to improve progress tracking and dialog management.

--- app/src/main/java/de/davis/passwordmanager/dialog/LoadingDialog.java
+++ app/src/main/java/de/davis/passwordmanager/dialog/LoadingDialog.java
@@ -5,27 +5,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 
 import de.davis.passwordmanager.databinding.LoadingLayoutBinding;
 
 public class LoadingDialog extends BaseDialogBuilder<LoadingDialog> {
 
     private LoadingLayoutBinding binding;
+    private AlertDialog alertDialog;
 
     public LoadingDialog(@NonNull Context context) {
         super(context);
         setCancelable(false);
     }
 
-    public void updateProgress(int current, int max){
-        double progress = (current * 100d / max);
+    public void dismiss(){
+        alertDialog.dismiss();
+    }
+
+    public void setMax(int max){
+        binding.progress.setMax(max);
+    }
+
+    public void updateProgress(int progress) {
+        binding.progress.setProgressCompat(progress, true);
         binding.progress.setIndeterminate(false);
-        binding.progress.setProgressCompat((int) progress, true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater) {
         binding = LoadingLayoutBinding.inflate(inflater);
         return binding.getRoot();
+    }
+
+    @NonNull
+    @Override
+    public AlertDialog create() {
+        return alertDialog = super.create();
     }
 }
