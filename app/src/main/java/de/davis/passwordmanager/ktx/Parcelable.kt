6a0ea903e41aa8add00f88c63034cde904b1c528
Davis <davis-wolfermann2005@gmx.de>
2024-02-04 02:06:27 +0100

    [Parcelable EXT] Fixed API 33 issue

--- app/src/main/java/de/davis/passwordmanager/ktx/Parcelable.kt
+++ app/src/main/java/de/davis/passwordmanager/ktx/Parcelable.kt
@@ -1,14 +1,16 @@
-@file:Suppress("DEPRECATION")
-
 package de.davis.passwordmanager.ktx
 
-import android.os.Build
 import android.os.Bundle
+import androidx.core.os.BundleCompat
 
 fun <A> Bundle.getParcelableCompat(key: String, clazz: Class<A>): A? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        classLoader = clazz.classLoader
-        getParcelable(key, clazz)
-    } else
-        getParcelable(key) as? A
+    classLoader = clazz.classLoader
+
+    // Due to issues with the new getParcelable API in Android API 33, BundleCompat.getParcelable
+    // is utilized here to ensure stable behavior across different Android versions. This workaround
+    // selectively applies the new getParcelable implementation for API levels 34 and above,
+    // addressing the reported bug.
+    // For more details, refer to the Google Issue Tracker: https://issuetracker.google.com/issues/274185314
+    // and the related discussion on Stack Overflow: https://stackoverflow.com/questions/76067109/getparcelable-crashes-due-to-null-iftable-in-agp-8
+    return BundleCompat.getParcelable(this, key, clazz)
 }
