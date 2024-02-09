dcc591f6d194ba4e5712fcfee26936b5e4a10d2e
Davis <davis-wolfermann2005@gmx.de>
2024-02-09 22:38:30 +0100

    [BackupOperation] Made Parcelable

--- app/src/main/java/de/davis/passwordmanager/backup/BackupOperation.kt
+++ app/src/main/java/de/davis/passwordmanager/backup/BackupOperation.kt
@@ -1,6 +1,10 @@
 package de.davis.passwordmanager.backup
 
-enum class BackupOperation {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+enum class BackupOperation : Parcelable {
     IMPORT,
     EXPORT
 }
