f2ae735adc4b5b2d9597adc2d95f25fd5457b84d
Davis <davis-wolfermann2005@gmx.de>
2025-05-20 14:08:20 +0200

    [Item] Fix viewmodel

--- app/src/main/kotlin/de/davis/keygo/item/di/ItemModule.kt
+++ app/src/main/kotlin/de/davis/keygo/item/di/ItemModule.kt
@@ -4,10 +4,11 @@ import de.davis.keygo.item.domain.usecase.EstimatePasswordStrengthUseCase
 import de.davis.keygo.item.presentation.password.PasswordViewModel
 import me.gosimple.nbvcxz.Nbvcxz
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val itemModule = module {
-    singleOf(::PasswordViewModel)
+    viewModelOf(::PasswordViewModel)
 
     single { Nbvcxz() }
     singleOf(::EstimatePasswordStrengthUseCase)
