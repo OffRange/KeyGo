ff0cd2f427f0b240d60393a2ed0aaf3a779f04a1
Davis <davis-wolfermann2005@gmx.de>
2025-05-27 09:43:19 +0200

    [Core] Use dsl

--- app/src/main/kotlin/de/davis/keygo/core/data/mapper/MainPasswordMapper.kt
+++ app/src/main/kotlin/de/davis/keygo/core/data/mapper/MainPasswordMapper.kt
@@ -1,18 +1,18 @@
 package de.davis.keygo.core.data.mapper
 
-import com.google.protobuf.Timestamp
+import com.google.protobuf.timestamp
 import de.davis.keygo.core.data.local.model.ProtoMainPassword
+import de.davis.keygo.core.data.local.model.protoMainPassword
 import de.davis.keygo.core.domain.model.MainPassword
 import java.time.Instant
 
 fun ProtoMainPassword.toDomain() =
     MainPassword(hash, Instant.ofEpochSecond(createdAt.seconds, createdAt.nanos.toLong()))
 
-fun MainPassword.toProto(): ProtoMainPassword = ProtoMainPassword.newBuilder().apply {
+fun MainPassword.toProto(): ProtoMainPassword = protoMainPassword {
     hash = this@toProto.hash
-    setCreatedAt(
-        Timestamp.newBuilder()
-            .setSeconds(this@toProto.createdAt.epochSecond)
-            .setNanos(this@toProto.createdAt.nano)
-    )
-}.build()
+    createdAt = timestamp {
+        seconds = this@toProto.createdAt.epochSecond
+        nanos = this@toProto.createdAt.nano
+    }
+}
