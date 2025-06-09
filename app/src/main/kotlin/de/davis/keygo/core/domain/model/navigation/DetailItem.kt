48fad90c2bbc753a3c3467e22eab97aefb3871b4
Davis Wolfermann <davis-wolfermann2005@gmx.de>
2025-06-09 13:19:37 +0200

    [Core] Use Parcelables as a workaround for b/378882434
    - violates domain-logic concepts

--- app/src/main/kotlin/de/davis/keygo/core/domain/model/navigation/DetailItem.kt
+++ app/src/main/kotlin/de/davis/keygo/core/domain/model/navigation/DetailItem.kt
@@ -1,10 +1,49 @@
 package de.davis.keygo.core.domain.model.navigation
 
-import de.davis.keygo.core.domain.model.VaultItem
+import android.os.Parcel
+import android.os.Parcelable
 import de.davis.keygo.generated.item.VaultItemEnum
 
 // Todo(b/378882434): Configuration changes will cause a crash as android can not save the state yet
-sealed interface DetailItem {
-    data class Edit(val type: VaultItemEnum) : DetailItem
-    data class View(val item: VaultItem) : DetailItem
+// To fix this, we use Parcelable even this violates the concept of domain-layer logic
+sealed interface DetailItem : Parcelable {
+    data class Edit(val type: VaultItemEnum) : DetailItem {
+        constructor(parcel: Parcel) : this(VaultItemEnum.entries[parcel.readInt()])
+
+        override fun describeContents(): Int = 0
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            dest.writeInt(type.ordinal)
+        }
+
+        companion object CREATOR : Parcelable.Creator<Edit> {
+            override fun createFromParcel(parcel: Parcel): Edit {
+                return Edit(parcel)
+            }
+
+            override fun newArray(size: Int): Array<Edit?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+
+    data class View(val itemId: Long) : DetailItem {
+        constructor(parcel: Parcel) : this(parcel.readLong())
+
+        override fun describeContents(): Int = 0
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            dest.writeLong(itemId)
+        }
+
+        companion object CREATOR : Parcelable.Creator<View> {
+            override fun createFromParcel(parcel: Parcel): View {
+                return View(parcel)
+            }
+
+            override fun newArray(size: Int): Array<View?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
 }
