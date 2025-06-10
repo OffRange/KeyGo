66687c80c47ebf662968ba8847b296eedc1cf30e
Davis <davis-wolfermann2005@gmx.de>
2025-06-10 23:06:51 +0200

    [Dashboard] Show selection only in single pane mode

--- app/src/main/kotlin/de/davis/keygo/dashboard/presentation/DashboardContent.kt
+++ app/src/main/kotlin/de/davis/keygo/dashboard/presentation/DashboardContent.kt
@@ -50,6 +50,7 @@ import de.davis.keygo.core.domain.model.Password
 import de.davis.keygo.core.domain.model.VaultSearchResult
 import de.davis.keygo.core.domain.model.crypto.CryptographicData
 import de.davis.keygo.core.domain.model.navigation.DetailItem
+import de.davis.keygo.core.presentation.LocalIsInSinglePaneMode
 import de.davis.keygo.core.presentation.component.KeyGoCard
 import de.davis.keygo.core.presentation.component.KeyGoCardProp
 import de.davis.keygo.core.presentation.navigation.LocalNavigator
@@ -232,7 +233,7 @@ fun DashboardContent(uiState: DashboardUIState, onEvent: (DashboardUIEvent) -> U
                         KeyGoLazyColumn(
                             items = uiState.items,
                             selectedItemIds = uiState.selectedItemIds,
-                            openedItemId = uiState.openedItemId,
+                            openedItemId = if (LocalIsInSinglePaneMode.current) -1L else uiState.openedItemId,
                             onDeleteRequest = {
                                 onEvent(DashboardUIEvent.OnDeleteRequest(it))
                             },
