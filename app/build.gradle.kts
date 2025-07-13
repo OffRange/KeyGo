d226d084be1d38f6a396c4c81194d331b6fc03c9
Davis <davis-wolfermann2005@gmx.de>
2025-07-13 18:42:49 +0200

    [Android] Enable minify and res shrinking in release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
