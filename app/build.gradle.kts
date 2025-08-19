21e5a98807afecc872874ce0a94ac17e45408168
Davis Wolfermann <davis-wolfermann2005@gmx.de>
2025-08-19 21:16:29 +0200

    [Gradle] Updated versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,21 +3,21 @@ plugins {
     id("com.google.android.gms.oss-licenses-plugin")
     kotlin("android") // org.jetbrains.kotlin.android
     kotlin("plugin.parcelize")
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17"
-    id("androidx.room") version "2.6.0"
-    id("com.google.protobuf") version "0.9.4"
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2"
+    id("androidx.room") version "2.7.2"
+    id("com.google.protobuf") version "0.9.5"
 }
 
 android {
     namespace = "de.davis.passwordmanager"
-    compileSdk = 34
+    compileSdk = 36
 
     val runTasks = gradle.startParameter.taskNames
     val isDebugBuild = runTasks.any { it.contains("assembleDebug") }
-    val build = 98 // alpha 0-31 | beta 32-63 | rc 64-95 | stable 96-99
+    val build = 96 // alpha 0-31 | beta 32-63 | rc 64-95 | stable 96-99
     val major = 1
     val minor = 2
-    val patch = 0
+    val patch = 1
 
     // xxyyyzzbb
     val vCode = if (isDebugBuild) 10 else major * 10_000_000 + minor * 10_000 + patch * 100 + build
@@ -33,11 +33,12 @@ android {
             else -> createSuffix("alpha")
         }
     }"
+    println("Computed version name: $vName")
 
     defaultConfig {
         applicationId = "de.davis.passwordmanager"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 36
         versionCode = vCode
         versionName = vName
 
@@ -63,6 +64,7 @@ android {
 
             versionNameSuffix = "-dev"
             applicationIdSuffix = ".dev"
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
@@ -101,24 +103,24 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.autofill:autofill:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.autofill:autofill:1.3.0")
 
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 
-    implementation("com.google.android.gms:play-services-oss-licenses:17.0.1")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("com.google.android.gms:play-services-oss-licenses:17.2.2")
+    implementation("com.google.code.gson:gson:2.13.1")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.recyclerview:recyclerview-selection:1.2.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.3")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.biometric:biometric:1.1.0")
-    implementation("androidx.browser:browser:1.7.0")
+    implementation("androidx.browser:browser:1.9.0")
     implementation("me.gosimple:nbvcxz:1.5.1")
     implementation("at.favre.lib:bcrypt:0.10.2")
     implementation("com.github.alvinhkh:TextDrawable:558677ea31")
@@ -126,26 +128,26 @@ dependencies {
     implementation("net.grey-panther:natural-comparator:1.1")
 
     implementation("com.github.keemobile:kotpass:0.6.1")
-    implementation("com.squareup.okio:okio:3.4.0") //Needed for Meta for kotpass
+    implementation("com.squareup.okio:okio:3.16.0") //Needed for Meta for kotpass
 
-    implementation("androidx.datastore:datastore:1.0.0")
-    implementation("com.google.protobuf:protobuf-kotlin-lite:3.25.2")
+    implementation("androidx.datastore:datastore:1.1.7")
+    implementation("com.google.protobuf:protobuf-kotlin-lite:4.32.0")
 
-    "githubImplementation"("com.squareup.retrofit2:retrofit:2.9.0")
-    "githubImplementation"("org.kohsuke:github-api:1.318")
+    "githubImplementation"("com.squareup.retrofit2:retrofit:3.0.0")
+    "githubImplementation"("org.kohsuke:github-api:1.329")
 
-    implementation("com.opencsv:opencsv:5.9")
+    implementation("com.opencsv:opencsv:5.12.0")
 
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
-    androidTestImplementation("androidx.room:room-testing:2.6.1")
+    implementation("androidx.room:room-runtime:2.7.2")
+    implementation("androidx.room:room-ktx:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2")
+    androidTestImplementation("androidx.room:room-testing:2.7.2")
 
-    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
 
 room {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.7")
     }
 }
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.12.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
 }
