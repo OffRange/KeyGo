5a2267f354c5366ecbdd1224f4bf14b0ed411abe
Davis Wolfermann <davis-wolfermann2005@gmx.de>
2025-08-25 23:49:04 +0200

    [Core] Inject Session instead of AesKey

--- app/src/main/kotlin/de/davis/keygo/core/data/crypto/CryptographicScopeProviderImpl.kt
+++ app/src/main/kotlin/de/davis/keygo/core/data/crypto/CryptographicScopeProviderImpl.kt
@@ -3,16 +3,13 @@ package de.davis.keygo.core.data.crypto
 import de.davis.keygo.core.domain.Session
 import de.davis.keygo.core.domain.crypto.CryptographicScope
 import de.davis.keygo.core.domain.crypto.CryptographicScopeProvider
-import de.davis.keygo.core.domain.model.crypto.AesKey
-import org.koin.core.annotation.ScopeId
 import org.koin.core.annotation.Single
 
 @Single
 internal class CryptographicScopeProviderImpl(
-    @ScopeId(name = Session.SCOPE_ID)
-    private val aesKey: AesKey
+    private val session: Session
 ) : CryptographicScopeProvider {
 
     override suspend fun <R> scope(block: suspend CryptographicScope.() -> R): R =
-        CryptographicScopeImpl(aesKey).block()
+        CryptographicScopeImpl(session.scope.get()).block()
 }
