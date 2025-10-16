6dd90e4dc780a0c4c6fa215f57af4c1e1559ad55
Davis <42292083+OffRange@users.noreply.github.com>
2025-10-16 17:14:52 +0200

    Fix package typo

--- core/item/src/main/kotlin/de/davis/keygo/core/item/di/CoreItemModule.kt
+++ core/item/src/main/kotlin/de/davis/keygo/core/item/di/CoreItemModule.kt
@@ -4,5 +4,5 @@ import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 
 @Module
-@ComponentScan("de.davis.keygo.core_item.**")
+@ComponentScan("de.davis.keygo.core.item.**")
 object CoreItemModule
