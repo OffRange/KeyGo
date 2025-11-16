e14ad735469d0e9063ab77c99037e35c3b372f3c
Davis Wolfermann <davis-wolfermann2005@gmx.de>
2025-11-16 19:50:04 +0100

    [Core] [Security] Add passkey encryption key

--- core/security/src/main/kotlin/de/davis/keygo/core/security/domain/model/KeyInfo.kt
+++ core/security/src/main/kotlin/de/davis/keygo/core/security/domain/model/KeyInfo.kt
@@ -1,3 +1,8 @@
 package de.davis.keygo.core.security.domain.model
 
-data class KeyInfo(val id: String)
+data class KeyInfo(val id: String) {
+
+    companion object {
+        val PasskeyEncryptionKey = KeyInfo("passkey_encryption_key")
+    }
+}
