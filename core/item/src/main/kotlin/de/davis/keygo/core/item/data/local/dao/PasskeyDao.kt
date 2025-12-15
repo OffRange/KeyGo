2f10da2e3bf6b59cdfc6a6b54fb4f836c972682f
Davis <davis-wolfermann2005@gmx.de>
2025-12-15 18:51:44 +0100

    [Core] [Item] Remove unused dao function

--- core/item/src/main/kotlin/de/davis/keygo/core/item/data/local/dao/PasskeyDao.kt
+++ core/item/src/main/kotlin/de/davis/keygo/core/item/data/local/dao/PasskeyDao.kt
@@ -9,9 +9,6 @@ import de.davis.keygo.core.item.domain.alias.ItemId
 @Dao
 internal interface PasskeyDao {
 
-    @Query("SELECT * FROM PasskeyEntity WHERE password_id = :passwordId")
-    suspend fun getPasskeysForPassword(passwordId: ItemId): List<PasskeyEntity>
-
     @Insert
     suspend fun insertPasskey(passkey: PasskeyEntity)
 
