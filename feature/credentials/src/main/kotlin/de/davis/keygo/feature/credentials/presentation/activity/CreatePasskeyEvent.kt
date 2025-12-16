5997cdab63f91d85a40787d86b9bd36626fcf758
Davis <davis-wolfermann2005@gmx.de>
2025-12-16 23:55:33 +0100

    [Feature] [Credentials] Make internal

--- feature/credentials/src/main/kotlin/de/davis/keygo/feature/credentials/presentation/activity/CreatePasskeyEvent.kt
+++ feature/credentials/src/main/kotlin/de/davis/keygo/feature/credentials/presentation/activity/CreatePasskeyEvent.kt
@@ -1,6 +1,6 @@
 package de.davis.keygo.feature.credentials.presentation.activity
 
-sealed interface CreatePasskeyEvent {
+internal sealed interface CreatePasskeyEvent {
     data object Abort : CreatePasskeyEvent
     data object ShowList : CreatePasskeyEvent
     data class Finish(val responseJson: String) : CreatePasskeyEvent
