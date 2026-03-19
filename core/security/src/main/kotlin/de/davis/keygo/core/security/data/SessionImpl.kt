97c3c9a5105b87ce6c36c83df0d13e1d67b08287
Davis <davis-wolfermann2005@gmx.de>
2026-03-19 14:50:45 +0100

    [Core] [Security] Refactor Session

--- core/security/src/main/kotlin/de/davis/keygo/core/security/data/SessionImpl.kt
+++ core/security/src/main/kotlin/de/davis/keygo/core/security/data/SessionImpl.kt
@@ -3,18 +3,27 @@ package de.davis.keygo.core.security.data
 import de.davis.keygo.core.security.domain.Session
 import de.davis.keygo.core.security.domain.crypto.model.AesKey
 import org.koin.core.annotation.Single
+import javax.security.auth.DestroyFailedException
 
 @Single
 internal class SessionImpl : Session {
 
+    private var _dek: AesKey? = null
+
+    override val dek: AesKey
+        get() = _dek ?: throw IllegalStateException("No active session")
+
     override fun startSession(dek: AesKey) {
         endSession()
-        scope.declare(dek)
+        _dek = dek
     }
 
     override fun endSession() {
-        if (scope.isNotClosed()) {
-            scope.close()
+        try {
+            _dek?.key?.destroy()
+        } catch (_: DestroyFailedException) {
+            // Not all SecretKey implementations support destroy
         }
+        _dek = null
     }
 }
