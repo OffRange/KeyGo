f75f6c2e46360cda96bdae189c9963ec5624c2b7
Davis Wolfermann <davis-wolfermann2005@gmx.de>
2026-04-02 16:04:36 +0200

    [Core] [UI] Use MaterialExpressiveTheme

--- core/ui/src/main/kotlin/de/davis/keygo/core/ui/theme/Theme.kt
+++ core/ui/src/main/kotlin/de/davis/keygo/core/ui/theme/Theme.kt
@@ -6,7 +6,8 @@ import android.os.Build
 import androidx.annotation.ChecksSdkIntAtLeast
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
@@ -43,6 +44,7 @@ fun selectSchemeForContrast(isDark: Boolean): ColorScheme {
     } else return colorScheme
 }
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun KeyGoTheme(
     isDarkMode: Boolean = isSystemInDarkTheme(),
@@ -58,7 +60,7 @@ fun KeyGoTheme(
         else -> selectSchemeForContrast(isDarkMode)
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = KeyGoTypography,
         content = content
