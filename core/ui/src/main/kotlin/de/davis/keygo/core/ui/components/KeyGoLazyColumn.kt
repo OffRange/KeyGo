ba4ad4e9989bb23101f39da1f4e0d02f9d298703
Davis <davis-wolfermann2005@gmx.de>
2026-04-08 20:10:07 +0200

    [Core] [Ui] Add preview

--- core/ui/src/main/kotlin/de/davis/keygo/core/ui/components/KeyGoLazyColumn.kt
+++ core/ui/src/main/kotlin/de/davis/keygo/core/ui/components/KeyGoLazyColumn.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
@@ -36,6 +37,7 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 
@@ -261,3 +263,29 @@ private val ContainerColor
     @Composable get() = MaterialTheme.colorScheme.surfaceContainerHigh
 private val OpenedContainerColor
     @Composable get() = MaterialTheme.colorScheme.primaryContainer
+
+
+@Preview
+@Composable
+private fun KeyGoLazyColumnPreview() {
+    val items = remember {
+        List(100) {
+            KeyGoColumnItem(
+                header = HeaderContent.Letter('A' + it / 10),
+                title = "Item $it",
+                id = it,
+            )
+        }.sortedBy { (it.header as HeaderContent.Letter).char }
+    }
+
+    MaterialTheme {
+        Surface {
+            KeyGoColumn(
+                items = items,
+                onDelete = {},
+                onItemClick = {},
+                onItemLongClick = {},
+            )
+        }
+    }
+}
