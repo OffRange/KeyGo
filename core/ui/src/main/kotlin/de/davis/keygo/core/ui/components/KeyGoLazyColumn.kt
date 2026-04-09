fa601a0d12590ac3e44115bd2b575e6f40fa496a
Davis <davis-wolfermann2005@gmx.de>
2026-04-09 21:20:08 +0200

    [Core] [Ui] Adjust KeyGoLazyColumn header vertical offset

--- core/ui/src/main/kotlin/de/davis/keygo/core/ui/components/KeyGoLazyColumn.kt
+++ core/ui/src/main/kotlin/de/davis/keygo/core/ui/components/KeyGoLazyColumn.kt
@@ -167,7 +167,7 @@ fun <ID : Any> KeyGoColumn(
         val headerOffset by remember(listState, lastInGroupIndices, density) {
             derivedStateOf {
                 val offset = with(density) {
-                    IntOffset(16.dp.roundToPx(), 16.dp.roundToPx())
+                    IntOffset(16.dp.roundToPx(), 12.dp.roundToPx())
                 }
 
                 if (listState.firstVisibleItemIndex in lastInGroupIndices)
