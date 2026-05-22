e478e0d588a7432484f2e60ea6ffc2880a260a04
Davis <davis-wolfermann2005@gmx.de>
2026-05-22 13:28:05 +0200

    feat(data): Implement cc entity

---
+++ core/item/src/main/kotlin/de/davis/keygo/core/item/data/local/entity/CreditCardEntity.kt
@@ -0,0 +1,35 @@
+package de.davis.keygo.core.item.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import de.davis.keygo.core.item.domain.alias.ItemId
+import de.davis.keygo.core.item.domain.model.EncryptedPayload
+import java.time.YearMonth
+
+@Entity(
+    tableName = "credit_card",
+    foreignKeys = [
+        ForeignKey(
+            entity = ItemEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["id"],
+            onDelete = ForeignKey.CASCADE,
+        )
+    ],
+)
+internal data class CreditCardEntity(
+    @PrimaryKey
+    val id: ItemId,
+    val holder: String?,
+    @Embedded(prefix = "card_number_")
+    val cardNumber: EncryptedPayload,
+    @ColumnInfo(name = "last_numbers")
+    val lastNumbers: String,
+    @Embedded(prefix = "cvv_")
+    val cvv: EncryptedPayload?,
+    @ColumnInfo(name = "expiration_date")
+    val expirationDate: YearMonth
+)
