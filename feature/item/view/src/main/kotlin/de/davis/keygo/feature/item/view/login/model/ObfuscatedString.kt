e3aa7243dc5eb1a7e729334b7512948b5baba406
Davis <davis-wolfermann2005@gmx.de>
2026-05-29 12:45:21 +0200

    refactor(item-view): memoize obfuscated string calculation

--- feature/item/view/src/main/kotlin/de/davis/keygo/feature/item/view/login/model/ObfuscatedString.kt
+++ feature/item/view/src/main/kotlin/de/davis/keygo/feature/item/view/login/model/ObfuscatedString.kt
@@ -5,20 +5,19 @@ data class ObfuscatedString(
     val formatted: String = raw,
     val visibleSuffixDigits: Int = 0,
 ) {
-    val hidden: String
-        get() {
-            val totalDigits = formatted.count(Char::isDigit)
-            val reveal = if (totalDigits > visibleSuffixDigits) visibleSuffixDigits else 0
-            var digitsSeen = 0
-            return buildString(formatted.length) {
-                for (c in formatted) {
-                    if (c.isDigit()) {
-                        digitsSeen++
-                        append(if (totalDigits - digitsSeen < reveal) c else DEFAULT_OBFUSCATION_CHAR)
-                    } else append(c)
-                }
+    val hidden: String by lazy {
+        val totalDigits = formatted.count(Char::isDigit)
+        val reveal = if (totalDigits > visibleSuffixDigits) visibleSuffixDigits else 0
+        var digitsSeen = 0
+        buildString(formatted.length) {
+            for (c in formatted) {
+                if (c.isDigit()) {
+                    digitsSeen++
+                    append(if (totalDigits - digitsSeen < reveal) c else DEFAULT_OBFUSCATION_CHAR)
+                } else append(c)
             }
         }
+    }
 
     companion object {
         private const val DEFAULT_OBFUSCATION_CHAR: Char = '•'
