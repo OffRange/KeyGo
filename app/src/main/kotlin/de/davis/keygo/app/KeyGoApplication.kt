aec385b5bab9f42b94af3d667b36ca9baa8f977b
Davis Wolfermann <offrange.developer@gmail.com>
2026-08-01 19:59:26 +0200

    feat(activity): add secure flag to every activity

--- app/src/main/kotlin/de/davis/keygo/app/KeyGoApplication.kt
+++ app/src/main/kotlin/de/davis/keygo/app/KeyGoApplication.kt
@@ -1,6 +1,9 @@
 package de.davis.keygo.app
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.view.WindowManager
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.workmanager.koin.workManagerFactory
@@ -17,5 +20,21 @@ class KeyGoApplication : Application() {
             androidContext(this@KeyGoApplication)
             workManagerFactory()
         }
+
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, p1: Bundle?) {
+                activity.window.setFlags(
+                    WindowManager.LayoutParams.FLAG_SECURE,
+                    WindowManager.LayoutParams.FLAG_SECURE
+                )
+            }
+
+            override fun onActivityDestroyed(p0: Activity) {}
+            override fun onActivityPaused(p0: Activity) {}
+            override fun onActivityResumed(p0: Activity) {}
+            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {}
+            override fun onActivityStarted(p0: Activity) {}
+            override fun onActivityStopped(p0: Activity) {}
+        })
     }
 }
