6b579a3533810473265eb0a4928eb7e8a9a4fe2e
Davis Wolfermann <offrange.developer@gmail.com>
2026-08-03 23:03:27 +0200

    feat(onboarding): add module

---
+++ feature/onboarding/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.keygo.android.compose)
+}
+
+android {
+    namespace = "de.davis.keygo.feature.onboarding"
+}
+
+dependencies {
+}
---
+++ feature/onboarding/src/main/kotlin/de/davis/keygo/feature/onboarding/di/FeatureOnboardingModule.kt
@@ -0,0 +1,10 @@
+package de.davis.keygo.feature.onboarding.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Configuration
+import org.koin.core.annotation.Module
+
+@Module
+@Configuration
+@ComponentScan("de.davis.keygo.feature.onboarding")
+object FeatureOnboardingModule
